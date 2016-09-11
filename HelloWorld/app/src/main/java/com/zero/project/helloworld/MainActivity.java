0664ba4a150ef8f2ea2dd9f398198b9b5e112cf6
gopinath0332 <gopinath0332@outlook.com>
2016-09-11 23:52:19 +0530

    Adding UI for content providers

--- HelloWorld/app/src/main/java/com/zero/project/helloworld/MainActivity.java
+++ HelloWorld/app/src/main/java/com/zero/project/helloworld/MainActivity.java
@@ -8,72 +8,77 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 
 import com.zero.project.helloworld.service.MyService;
 
 public class MainActivity extends AppCompatActivity {
 
-    public final String MESSAGE = "Android: ";
+    public final String TAG = "Android: ";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d(MESSAGE, "onCreate event");
+        Log.d(TAG, "onCreate event");
     }
 
     public void startService(View view) {
-        Log.d(MESSAGE, "service started ");
+        Log.d(TAG, "service started ");
         startService(new Intent(getBaseContext(), MyService.class));
     }
 
-    public void broadcastIntent(View view){
-        Log.d(MESSAGE, "broadcasting Intent: ");
+    public void broadcastIntent(View view) {
+        Log.d(TAG, "broadcasting Intent: ");
         Intent intent = new Intent();
         intent.setAction("com.zero.project.CUSTOM_EVENT");
         sendBroadcast(intent);
     }
 
     public void stopService(View view) {
-        Log.d(MESSAGE, "stop Service: ");
+        Log.d(TAG, "stop Service: ");
         stopService(new Intent(getBaseContext(), MyService.class));
     }
 
+    public void addName(View view) {
+        EditText editText = (EditText)findViewById(R.id.std_name);
+        Log.d(TAG, "addName: "+editText.getText());
+    }
 
     //Activity Life cycle
     @Override
     protected void onStart() {
         super.onStart();
-        Log.d(MESSAGE, "onStart event");
+        Log.d(TAG, "onStart event");
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        Log.d(MESSAGE, "onResume event");
+        Log.d(TAG, "onResume event");
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        Log.d(MESSAGE, "onPause event");
+        Log.d(TAG, "onPause event");
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        Log.d(MESSAGE, "onStop event");
+        Log.d(TAG, "onStop event");
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        Log.d(MESSAGE, "onDestroy event");
+        Log.d(TAG, "onDestroy event");
     }
 
     @Override
     protected void onRestart() {
         super.onRestart();
-        Log.d(MESSAGE, "onRestart event");
+        Log.d(TAG, "onRestart event");
     }
 }
