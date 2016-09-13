75cab66be30b372ed4cd252e4e1bad2c4089dae6
gopinath0332 <gopinath0332@outlook.com>
2016-09-13 23:05:44 +0530

    Adding activity callback

--- HelloWorld/app/src/main/java/com/zero/project/helloworld/IntentActivity.java
+++ HelloWorld/app/src/main/java/com/zero/project/helloworld/IntentActivity.java
@@ -1,7 +1,10 @@
 package com.zero.project.helloworld;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class IntentActivity extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class IntentActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intent);
+    }
+
+    public void openBrowser(View view) {
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.facebook.com/"));
+        startActivity(intent);
+    }
+
+    public void openPhone(View view) {
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("tel:9944904257"));
+        startActivity(intent);
     }
 }
