5fff99890ea9804b678d37e8d9f64dd3b520a0b7
gopinath0332 <gopinath0332@outlook.com>
2016-09-14 23:31:15 +0530

    Adding message activity

--- MyFirstApp/app/src/main/java/com/zero/project/myfirstapp/MainActivity.java
+++ MyFirstApp/app/src/main/java/com/zero/project/myfirstapp/MainActivity.java
@@ -1,13 +1,29 @@
 package com.zero.project.myfirstapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String EXTRA_MESSAGE = "com.zero.project.message";
+    public final String TAG = "Main Activity: ";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void sendMessage(View view) {
+        Log.d(TAG, "sendMessage: ");
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 }
